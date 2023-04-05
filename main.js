0351759a23390b55eb969b222e3c95b9451f28fd
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-04-04 21:49:04 -0400

    Almost Complete

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+const selectMenu = document.querySelector("#titles")
+const options = document.querySelector("option")
+const displayInfo = document.querySelector("#display-info")
+const submitButton = document.getElementById("input")
+const reviewList = document.querySelector("#review-list")
 
 
 
@@ -7,7 +11,53 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+fetch("https://resource-ghibli-api.onrender.com/films/")
+.then((response) => response.json())
+.then((data) => {
+const movies = data
+let currentMovie = null
+    populateSelectMovie(data)
+   
+    selectMenu.addEventListener("change", (event) =>{
+
+        for (const movie of movies) {
+            if(movie.id === selectMenu.value)
+            displayInfo.innerHTML= ` <h3>${movie.title}</h3>
+            <p>${movie.release_date}</p>
+            <p>${movie.description}</p>`
+        }
+    })
+
+
+submitButton.addEventListener ("click", (event) =>{
+
+    event.preventDefault()
+    let newReview = document.createElement("li")
+    newReview.innerHTML += `<strong>${currentMovie.title}</strong>` + " -- "
+    newReview.innerHTML += commentInput.value
+    commentList.append(newReview)
+
+})
+
+})
+.catch(error => {console.log(error)})
+
+
+let populateSelectMovie = movies => {
+    movies.forEach(movie => {
+        let newOption = document.createElement("option")
+        newOption.value = movie.id
+        newOption.textContent = movie.title
+        selectMenu.append(newOption)
+    })
+}
+
+
+
+
+
+
+
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
